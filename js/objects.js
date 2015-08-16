119f2a4bcbc555f64b7e66e808e023b20c265ab7
Zoran Markovic <markonimobile@gmail.com>
2015-08-16 22:22:45 +0200

    JavaScipt - update objects.js

--- js/objects.js
+++ js/objects.js
@@ -6,12 +6,12 @@ Read my post JavaScript Objects in Detail http://javascriptissexy.com/javascript
 Or read chapter 6 of JavaScript: The Definitive Guide.
 Or read chapter 6 of Professional JavaScript for Web Developers. NOTE: Only read the "Understanding Objects" section.
 Any of the 3 is fine, although the 2 textbooks go into more detail: the extra detail you can skip confidently, if you read and thoroughly understand my post.
-For more detail check my quide >>> https://docs.google.com/document/d/15dGt4mrRw5EQbd6UAjydGpjMe1I_h5rGJq4yHtX-9zk/edit?usp=sharing
+For more detail check quide by Zoki >>> https://docs.google.com/document/d/15dGt4mrRw5EQbd6UAjydGpjMe1I_h5rGJq4yHtX-9zk/edit?usp=sharing
 
 JS ima jedan komplekasan tip podatka a to je Objekat i on je promenljiv (mutable)
 ima 5 prostih tipova podataka: Number, String, Boolean,Undefined i Null (oni su immutable - nepromenjivi su)
 
-Objekat je neuredjena lista primitivnih tipova podataka, referenci koji se cuvaju 
+Objekat je neuredjena lista primitivnih tipova podataka, referenci koji se cuvaju
 kao serija ime-vrednost parova koji se nazivaju property, dok se funkcije zovu metodi.
 */
 
@@ -57,19 +57,19 @@ Ove atribute ne koristimo u praksi ali dobro je da se znaju i setovani su na tru
 
 	//======	Objekat literal
 	var myObj = {} // prazan objekat
-	
+
 	var mango = {
 		color: "yellow",
-		shape: "round", 
+		shape: "round",
 		sweetness: 8,
-		
+
 		howSweetAmI: function () {
 			console.log("Hmm Hmm Good");
 		}
 	};
 
-	//======	Objekat konstruktor 
-	// Konstruktor je funkcija koja se koristi za inicijalizovanje novih objekata. 
+	//======	Objekat konstruktor
+	// Konstruktor je funkcija koja se koristi za inicijalizovanje novih objekata.
 	//Koristi se === new === kljucna rec za pozivanje konstruktora.
 
 	var mango = new Object();
@@ -83,7 +83,7 @@ Ove atribute ne koristimo u praksi ali dobro je da se znaju i setovani su na tru
 
 //======	Paterni za kreiranje objekata: 	======
 
-	//Za proste objekte, ili objekate koji ce se mozda samo jednom koristiti, 
+	//Za proste objekte, ili objekate koji ce se mozda samo jednom koristiti,
 	//2 navedena metoda za kreiranje objekata gorenavedena  su i vise nego dovoljna.
 
 	//======	Funkcija konstruktor patern za kreiranje objekata =====
@@ -93,7 +93,7 @@ Ove atribute ne koristimo u praksi ali dobro je da se znaju i setovani su na tru
 		2. Assign the === this === value of the constructor to the new object (so this points to the new object).
 		3. Execute the code inside the constructor (adds properties to the new object).
 		4. Return the new object.
-		======= Vazno ========
+		======= Vazno MANA ========
 		Constructors defined in this manner are defined on the Global object (the window object in web browsers), which is not good
 		*/
 
@@ -113,32 +113,32 @@ Ove atribute ne koristimo u praksi ali dobro je da se znaju i setovani su na tru
 	 	//The only difference between constructor functions and other functions is the way in which they are called.
 	 	//Example:
 			 	//use as a constructor
-				var person = new Person1("Nicholas", "Software Engineer", 29); 
+				var person = new Person1("Nicholas", "Software Engineer", 29);
 				//person.showNanme(); //"Nicholas"
-				
+
 				//call as a function
-				Person1("Greg", "Doctor", 27); //adds to window 
+				Person1("Greg", "Doctor", 27); //adds to window
 				//window.showNanme(); //"Greg"
 
-				//call in the scope of another object 
+				//call in the scope of another object
 				var obj1 = new Object();
-				Person1.call(obj1, "Kristen", "Nurse", 25); 
+				Person1.call(obj1, "Kristen", "Nurse", 25);
 				//obj1.showNanme(); //"Kristen"
 				//print(obj1.age);
 
 	/*
-	The first part of this example shows the typical use of a constructor, to create a = new = object via the new operator. 
-	The second part shows what happens when the Person() function is called without the new operator: the properties and methods get added to the window object. 
-	Remember that the this object always points to the Global object (window in web browsers) when a function is called without an explicitly set this value 
-	(by being an object method or through call()/apply()). 
-	So after the function is called, the sayName() method can be called on the window object, and it will return “Greg”. 
-	The Person() function can also be called within the scope of a particular object using call() (or apply()). In this case, it’s called with a this value of the object o, which then gets assigned 
+	The first part of this example shows the typical use of a constructor, to create a = new = object via the new operator.
+	The second part shows what happens when the Person() function is called without the new operator: the properties and methods get added to the window object.
+	Remember that the =this= object always points to the Global object (window in web browsers) when a function is called without an explicitly set this value
+	(by being an object method or through call()/apply()).
+	So after the function is called, the sayName() method can be called on the window object, and it will return “Greg”.
+	The Person() function can also be called within the scope of a particular object using call() (or apply()). In this case, it’s called with a this value of the object o, which then gets assigned
 	all of the properties and the sayName() method.
-	
+
 	VAZNO:
-	Though the constructor paradigm is useful, it is not without its faults. 
+	Though the constructor paradigm is useful, it is not without its faults.
 	The major downside to constructors is that methods are created once for each instance
-	
+
 	VAZNO:
 	Inherited propery je definisan na objekat prototype poropertiju.
 	someObject.firstName = "Zoki";
@@ -151,7 +151,7 @@ Ove atribute ne koristimo u praksi ali dobro je da se znaju i setovani su na tru
 
 
 	//======	Prototype patern za kreiranje objekata ====
-	
+
 	//The benefit of using the prototype is that all of its properties and methods are shared among object instances
 	function Person2(){}
 	Person2.prototype.name = "Zoran";
@@ -173,61 +173,118 @@ Ove atribute ne koristimo u praksi ali dobro je da se znaju i setovani su na tru
 	// Svaki objekat lireral  je povezan sa Object.prototype od kog nasledjuje sve njegove propertije!
 	// Kada se pravi novi objekat, moze se birati prototype objekat od kog ce naslediti sve sto treba!
 	// Kada pravimo izmene na nasem objektu, objekat prototype je nedirnut!
-	// Link protototypa se koristi samo za preuzimanje. 
-	// DELEGATION - Ako objekat nema property, onda JS gleda u njegov prototype, ako ga i on nema, onda ide dalje na gore do Objct.prototype. 
+	// Link protototypa se koristi samo za preuzimanje.
+	// DELEGATION - Ako objekat nema property, onda JS gleda u njegov prototype, ako ga i on nema, onda ide dalje na gore do Objct.prototype.
 	//              Ako ga ni on nema onda vraca undefined.
 
-	//The benefit of using the prototype is that all of its properties and methods are shared among object instances. 
-	//Instead of assigning object information in the constructor, they can be assigned directly to the prototype, 
+	//The benefit of using the prototype is that all of its properties and methods are shared among object instances.
+	//Instead of assigning object information in the constructor, they can be assigned directly to the prototype,
 
 
-	//===== The isPrototypeOf() method ==== 
-	//It can be used to determine if this relationship exists between objects. 
-	//Essentially, isPrototypeOf() returns true if [[Prototype]] points to the prototype on which the method is being called, 
+	//================================================================================================
+	//======	Combination Constructor/Prototype Pattern	=====	NAJBOLJI	===================================
+	//================================================================================================
+	/*
+	The =constructor pattern= defines instance properties,
+	whereas the =prototype pattern= defines methods and shared properties.
+
+	With this approach, each instance ends up with its own copy of the instance properties,
+	but they all share references to methods, conserving memory.
+
+	This pattern allows arguments to be passed into the constructor as well, effectively combining the best parts of each pattern.
+	*/
+
+	function Persons (name, age, job){
+		this.name = name;
+		this.age = age;
+		this.job = job;
+		this.friends = ['Mila', 'Nenad'];
+	}
+
+	Persons.prototype = {
+		constructor:Persons,
+		sayName: function(){
+			print(this.name);
+		}
+	};
+
+	var p1 = new Persons("Zoran", 32, "Developer");
+	var p2 = new Persons("Jeka", 29, "Pravnik");
+
+	p1.friends.push("Dane");
+
+	//print(p1.friends); //”Shelby,Court,Van”
+	//print(p2.friends); //”Shelby,Court”
+	//print(p1.friends === p2.friends); //false
+	//print(p1.sayName === p2.sayName); //true
+
+/* ZAKLJUCAK:
+	Note that the instance properties are now defined solely in the constructor,
+	and the shared property constructor and the method sayName() are defined on the prototype.
+
+	When person1.friends is augmented by adding a new string, person2.friends is not affected,
+	because they each have separate arrays.
+
+	The hybrid constructor/prototype pattern is the most widely used and accepted practice for defining custom reference types in ECMAScript.
+	Generally speaking, this is the default pattern to use for defining reference types.
+*/
+
+//====================================================
+// Object’s Prototype Attribute and Prototype Property
+// The prototype attribute and prototype property of an object are critically important concepts to understand in JavaScript.
+// Read my post JavaScript Prototype in Plain, Detailed Language for more. 
+// http://javascriptissexy.com/javascript-prototype-in-plain-detailed-language/
+
+
+//======================== PROTOTYPE METHODS ========================
+
+//===== The isPrototypeOf() method ====
+	//It can be used to determine if this relationship exists between objects.
+	//Essentially, isPrototypeOf() returns true if [[Prototype]] points to the prototype on which the method is being called,
 	//as shown here:
-		//alert(Person2.prototype.isPrototypeOf(p1)); //true 
+		//alert(Person2.prototype.isPrototypeOf(p1)); //true
 		//alert(Person2.prototype.isPrototypeOf(p2)); //true
 
 
 	//==== Object.getPrototypeOf() ====
-	//It returns the =value= of [[Prototype]] in all supporting implementations. 
-	//Using Object.getPrototypeOf(), you are able to retrieve an object’s prototype easily, 
+	//It returns the =value= of [[Prototype]] in all supporting implementations.
+	//Using Object.getPrototypeOf(), you are able to retrieve an object’s prototype easily,
 	//which becomes important once you want to implement inheritance using the prototype!!!
 	//For example:
-		//alert(Object.getPrototypeOf(p1) == Person2.prototype); //true 
+		//alert(Object.getPrototypeOf(p1) == Person2.prototype); //true
 		//alert(Object.getPrototypeOf(p1).name); //"Zoran"
 
 
-	//===== REFLECTION 	The hasOwnProperty() method ==== 
-	//This method, which is inherited from Object, returns true only if a property of the given name exists on the object instance, 
+	//===== REFLECTION 	The hasOwnProperty() method ====
+	//This method, which is inherited from Object, returns true only if a property of the given name exists on the object instance,
 	// as in this example:
 
 	function Person3(){ }
-	Person3.prototype.name = "Nicholas"; 
-	Person3.prototype.age = 29; 
-	Person3.prototype.job = "Software Engineer"; 
+	Person3.prototype.name = "Nicholas";
+	Person3.prototype.age = 29;
+	Person3.prototype.job = "Software Engineer";
 	Person3.prototype.sayName = function(){
-		print(this.name); 
+		print(this.name);
 	};
 
-	var person1 = new Person3(); 
+	var person1 = new Person3();
 	var person2 = new Person3();
 
 	//alert(person1.hasOwnProperty("name"));  //false because name exsists in prototype
 
 	person1.name = 'Greg';
-	//alert(person1.name); //Greg - from instance 
+	//alert(person1.name); //Greg - from instance
 	//alert(person1.hasOwnProperty('name')); //true
 
 	//alert(person2.name); //Nicholas - from prototype
     //alert(person2.hasOwnProperty('name'));  //false
 
     delete person1.name;
-    //alert(person1.name); //Nicholas - from the prototype 
+    //alert(person1.name); //Nicholas - from the prototype
     //alert(person1.hasOwnProperty(“name”)); //false
 
-    //==== IN operator return TRUE if a property of the given name exists on the object instance or prototype, 
-    //It’s possible to determine if the property of an object exists on the prototype 
+    //==== IN operator return TRUE if a property of the given name exists on the object instance or prototype,
+    //It’s possible to determine if the property of an object exists on the prototype
     //by combining a call to hasOwnProperty() with the in operator like this:
     	function hasPrototypeProperty(object, name){
     		return !object.hasOwnProperty(name) && (name in object);
@@ -249,31 +306,32 @@ Ove atribute ne koristimo u praksi ali dobro je da se znaju i setovani su na tru
 
 //===== VAZNO ===== VAZNO ===== ==============================================
 //====== Problems with Prototypes =====
-	//The prototype pattern isn’t without its faults. 
+	//The prototype pattern isn’t without its faults.
 	//For one, it negates the ability to pass initialization arguments into the constructor, meaning that all instances get the same property values by default.
 	//The main problem comes with their shared nature - The real problem occurs when a property contains a reference value.
 
-	function Person4(){ 
+	function Person4(){
 	}
-	
-	Person4.prototype = { 
-		constructor: Person4, 
-		name : 'Nicholas', 
+
+	Person4.prototype = {
+		constructor: Person4,
+		name : 'Nicholas',
 		age : 29,
 		job : 'Software Engineer',
 	    friends : ['Shelby', 'Court'],
-		sayName : function () { 
+		sayName : function () {
 			alert(this.name);
-		} 
+		}
 	};
-	
-	var person1 = new Person4(); 
+
+	var person1 = new Person4();
 	var person2 = new Person4();
-	
+
 	person1.friends.push('Van');
 	//alert(person1.friends);    //”Shelby,Court,Van”
 	//alert(person2.friends);    //”Shelby,Court,Van” 		Sto je glupooooo!!!
 	//alert(person1.friends === person2.friends);  //true 	ne valja ovako!!!
+
 
 	//VIDI i OVO
 	//==========================================================================================
@@ -281,64 +339,8 @@ Ove atribute ne koristimo u praksi ali dobro je da se znaju i setovani su na tru
 	//==========================================================================================
 
 	/*
-	The prototype attribute and prototype property of an object are critically important concepts 
-	to understand in JavaScript. 
+	The prototype attribute and prototype property of an object are critically important concepts
+	to understand in JavaScript.
 	Read my post JavaScript Prototype in Plain, Detailed Language for more. 
 	http://javascriptissexy.com/javascript-prototype-in-plain-detailed-language/
 	*/
-
-	
-	//================================================================================================
-	//======	Combination Constructor/Prototype Pattern	=====	NAJBOLJI	===================================
-	//================================================================================================
-	/*
-	The =constructor pattern= defines instance properties, 
-	whereas the =prototype pattern= defines methods and shared properties. 
-
-	With this approach, each instance ends up with its own copy of the instance properties, 
-	but they all share references to methods, conserving memory. 
-
-	This pattern allows arguments to be passed into the constructor as well, effectively combining the best parts of each pattern. 
-	*/
-
-	function Persons (name, age, job){
-		this.name = name;
-		this.age = age;
-		this.job = job;
-		this.friends = ['Mila', 'Nenad'];
-	}
-
-	Persons.prototype = {
-		constructor:Persons,
-		sayName: function(){
-			print(this.name);	
-		}
-	};
-
-	var p1 = new Persons("Zoran", 32, "Developer");
-	var p2 = new Persons("Jeka", 29, "Pravnik");
-
-	p1.friends.push("Dane");
-
-	//print(p1.friends); //”Shelby,Court,Van” 
-	//print(p2.friends); //”Shelby,Court” 
-	//print(p1.friends === p2.friends); //false 
-	//print(p1.sayName === p2.sayName); //true
-
-/* ZAKLJUCAK:
-	Note that the instance properties are now defined solely in the constructor, 
-	and the shared property constructor and the method sayName() are defined on the prototype. 
-
-	When person1.friends is augmented by adding a new string, person2.friends is not affected, 
-	because they each have separate arrays.
-
-	The hybrid constructor/prototype pattern is the most widely used and accepted practice for defining custom reference types in ECMAScript. 
-	Generally speaking, this is the default pattern to use for defining reference types.
-*/
-
-//====================================================
-// Object’s Prototype Attribute and Prototype Property
-// The prototype attribute and prototype property of an object are critically important concepts to understand in JavaScript. 
-// Read my post JavaScript Prototype in Plain, Detailed Language for more. 
-// http://javascriptissexy.com/javascript-prototype-in-plain-detailed-language/
-
