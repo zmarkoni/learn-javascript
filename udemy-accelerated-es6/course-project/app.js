5cbe516e4ede51d8547a27ce7d83548099b0d41f
Zoran Markovic <zoran.markovic@netcentric.biz>
2018-04-01 22:47:30 +0200

    use FETCH insted if XMLHttpRequest

--- udemy-accelerated-es6/course-project/app.js
+++ udemy-accelerated-es6/course-project/app.js
@@ -1,5 +1,5 @@
 import * as ELEMENTS from 'elements.js';
-import { Http } from 'http.js';
+import {Http} from 'http.js';
 import {WEATHER_PROXY_HANDLER, WeatherData} from "weather-data.js";
 
 const APP_ID = '9be67c0730dac6be80d27175db79343e';
@@ -7,24 +7,37 @@ const APP_ID = '9be67c0730dac6be80d27175db79343e';
 ELEMENTS.ELEMENT_SEARCH_BUTTON.addEventListener('click', searchWeather);
 
 function searchWeather() {
-   const CITY_NAME = ELEMENTS.ELEMENT_SEARCHED_CITY.value.trim();
-   if(CITY_NAME.length == 0) {
-       alert('Please enter city name');
-   }
+    const CITY_NAME = ELEMENTS.ELEMENT_SEARCHED_CITY.value.trim();
+    if (CITY_NAME.length == 0) {
+        alert('Please enter city name');
+    }
 
     ELEMENTS.ELEMENT_LOADING_TEXT.style.display = 'block';
     ELEMENTS.ELEMENT_WEATHER_BOX.style.display = 'none';
 
-   const URL = 'http://api.openweathermap.org/data/2.5/weather?q=' + CITY_NAME + '&units=metric&appid=' + APP_ID;
-   Http.fetchData(URL)
-       .then(responseData => {
-           const WEATHER_DATA = new WeatherData(CITY_NAME, responseData.weather[0].description.toUpperCase());
-           const WEATHER_POXY = new Proxy(WEATHER_DATA, WEATHER_PROXY_HANDLER);
-           WEATHER_POXY.temperature = responseData.main.temp;
+    const URL = 'http://api.openweathermap.org/data/2.5/weather?q=' + CITY_NAME + '&units=metric&appid=' + APP_ID;
+    // Http.fetchData(URL)
+    //     .then(responseData => {
+    //         const WEATHER_DATA = new WeatherData(CITY_NAME, responseData.weather[0].description.toUpperCase());
+    //         const WEATHER_POXY = new Proxy(WEATHER_DATA, WEATHER_PROXY_HANDLER);
+    //         WEATHER_POXY.temperature = responseData.main.temp;
+    //
+    //         updateWeather(WEATHER_POXY);
+    //     })
+    //     .catch(error => alert(error));
 
-           updateWeather(WEATHER_POXY);
-       })
-       .catch(error => alert(error));
+    // ===== Or we can use FETCH
+    fetch(URL)
+        .then(Http.status)
+        .then(Http.json)
+        .then(responseData => {
+            const WEATHER_DATA = new WeatherData(CITY_NAME, responseData.weather[0].description.toUpperCase());
+            const WEATHER_POXY = new Proxy(WEATHER_DATA, WEATHER_PROXY_HANDLER);
+            WEATHER_POXY.temperature = responseData.main.temp;
+
+            updateWeather(WEATHER_POXY);
+        })
+        .catch(error => alert(error));
 }
 
 function updateWeather(weatherData) {
--- udemy-accelerated-es6/course-project/http.js
+++ udemy-accelerated-es6/course-project/http.js
@@ -14,4 +14,19 @@ export class Http {
            HTTP.send();
         });
     }
+
+    // or we can use FETCH
+
+    static status(response) {
+        if(response.status >=200 && response.status < 300) {
+            return Promise.resolve(response)
+        }
+        else {
+            return Promise.reject(new Error(response.status))
+        }
+    }
+
+    static json(response) {
+        return response.json()
+    }
 }
