9be391ed08797e972197b3b16ade9ba49dd5aad8
Zoran Markovic <zoran.markovic@netcentric.biz>
2018-08-02 09:31:10 +0200

    create jasmine spy for callBack function

--- pluralsight-testing-javascript/m5-jasmine-spies-exercise-files/before/jasmine-function-spy/Spec.js
+++ pluralsight-testing-javascript/m5-jasmine-spies-exercise-files/before/jasmine-function-spy/Spec.js
@@ -5,7 +5,9 @@ function callMyCallback(cb) {
 describe("Spies", function() {
   
   it("should spy on a callback", function() {
-
+      var spyCB = jasmine.createSpy('myspay');
+      callMyCallback(spyCB);
+      expect(spyCB).toHaveBeenCalled();
   });
   
 });
