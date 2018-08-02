74f6c03793912e14f6871dfea065185123bc6245
Zoran Markovic <zoran.markovic@netcentric.biz>
2018-08-02 10:38:44 +0200

    Creating full spy Objects with methods

--- pluralsight-testing-javascript/m5-jasmine-spies-exercise-files/before/jasmine-createSpyObj/Spec.js
+++ pluralsight-testing-javascript/m5-jasmine-spies-exercise-files/before/jasmine-createSpyObj/Spec.js
@@ -1,10 +1,15 @@
-
+// Creating full spy Objects with methods
+// Instead of using spyOn over and over again
+// This is for Object which have dependencies, we can call createSpyObjects and test all Objects methods
 describe("Spies", function() {
-
 
 	it("should create a spy object", function() {
 		var spy = jasmine.createSpyObj('mySpy', ['getName', 'save']);
+
+		// fake implementation of getName method
 		spy.getName.andReturn("bob");
+
+		// fake implementation of save method
 		spy.save.andCallFake(function() { console.log('save called');});
 
 		expect(spy.getName()).toEqual("bob");
