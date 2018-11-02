efca61d995f8771693c2174f44cd520173445b92
Zoran Markovic <zoran.markovic@netcentric.biz>
2018-11-02 15:10:23 +0100

    YDKJS configure linter

--- you-dont-know-js/src/scripts/index.js
+++ you-dont-know-js/src/scripts/index.js
@@ -1,3 +1,5 @@
 import '../styles/index.scss';
+import { test } from './scope-and-closures.js';
 
-console.log('webpack starterkit');
+console.log('webpack starterkit'); // eslint-disable-line no-undef
+console.log(test); // eslint-disable-line no-undef
