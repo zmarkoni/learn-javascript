395286bcaf3f0d291724cd4542bf02213a856629
Zoran Markovic <zoran.markovic@netcentric.biz>
2019-03-24 00:12:49 +0100

    udemy-accelerated-es6/udemy-es6/basics/10-destructuring-objects.js

--- udemy-accelerated-es6/udemy-es6/basics/10-destructuring-objects.js
+++ udemy-accelerated-es6/udemy-es6/basics/10-destructuring-objects.js
@@ -7,13 +7,25 @@ let obj = {
     },
     greet1: () => console.log('Hello ' + this.name)
 };
+//console.log(obj.name, obj.age);
 
 let {name, age, greet} = obj; // variable names has to match with obj properties names
-console.log(name, age);
 
-greet(); // "Hello"  , should be "Hello Max"
+//console.log(name, age);
+//greet(); // "Hello"  , should be "Hello Max"
 //obj.greet(); // "Hello Max"
 
 // we can use aliases
-let {greet: hello} = obj;
-hello();
+//let {greet: hello} = obj;
+//hello();
+
+
+//Object destructuring for parameters works, too:
+
+function foo( { x, y } ) {
+	console.log( x, y );
+}
+
+foo( { x: 1, y: 2 } );				// 2 1
+foo( { y: 42 } );					// undefined 42
+foo( {} );							// undefined undefined
