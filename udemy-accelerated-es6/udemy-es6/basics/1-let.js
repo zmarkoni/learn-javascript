4fc005658eabbbf27dea598d6158ece10dacc352
Zoran Markovic <zoran.markovic@netcentric.biz>
2019-03-23 21:23:40 +0100

    udemy-accelerated-es6/udemy-es6/basics/1-let.js

--- udemy-accelerated-es6/udemy-es6/basics/1-let.js
+++ udemy-accelerated-es6/udemy-es6/basics/1-let.js
@@ -2,9 +2,33 @@
 
 let age = 30; //global scope
 
-if(true) {
-  let age = 20; // block scope , different variable
-  console.log(age); //20
+// if(true) {
+//   let age = 20; // block scope , different variable
+//   console.log(age); //20
+// }
+//
+// console.log(age); // 30
+
+// Create block scope with immediate invoking function
+(function IIFE(){
+	var age = 3;
+	console.log( age );	// 3
+})();
+
+// or blok scope can be formed only with {} in ES6
+
+{
+    let age = 20; // block scope , different variable
+      console.log(age); //20
 }
 
 console.log(age); // 30
+
+// declaration of LET should always be at the of the block
+{
+	console.log( a );	// undefined
+	console.log( b );	// ReferenceError!
+
+	var a;
+	let b;
+}
